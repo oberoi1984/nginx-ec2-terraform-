79fbe550949926db4507f37536c05ae2e1f434b3
oberoi1984 <41862558+oberoi1984@users.noreply.github.com>
2024-10-11 00:59:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,16 +5,6 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
-#variable "key_name" {
-#  description = "Name of the key pair"
-#  default     = "nginx-key-pair"
-#}
-
-variable "public_key_path" {
-  description = "Path to the public key file"
-  default     = "~/.ssh/id_rsa.pub"
-}
-
 variable "ami_id" {
   description = "AMI ID of the EC2 instance"
   default     = "ami-078264b8ba71bc45e"  # Replace with an AMI ID available in your region
